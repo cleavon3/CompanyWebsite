a33a49b28477b56fcc3bddbf318600fbb79552cc
cleavon <cleavon.design.com>
2024-11-03 21:48:37 +0100

    my first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+st.header ("Company website")
+st.write("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+ incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+ exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+ dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia deserunt mollit anim id est laborum""")
+st.subheader("Our Team")
+
+col1, col2, col3 = st.columns(3)
+
+
+
+df = pandas.read_csv("data.csv")
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.image("images (1)/" +row["image"])
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
+
+
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.image("images (1)/" +row["image"])
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.image("images (1)/" +row["image"])
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
